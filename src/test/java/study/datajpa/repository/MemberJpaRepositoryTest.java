25a4bdc96bf1dfacf30a838f7e1a49217e971ccd
injinjeong <snowwwwww@naver.com>
2022-01-05 22:00:03 +0900

    pure jpa repository add

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -3,8 +3,12 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,6 +18,9 @@ class MemberJpaRepositoryTest {
 
     @Autowired
     MemberJpaRepository memberJpaRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -25,6 +32,73 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void memberCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
+    }
+
+    @Test
+    @Rollback(value = false)
+    public void teamCRUD() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+        memberJpaRepository.save(member3);
+        memberJpaRepository.save(member4);
+
+        Team findTeamA = teamRepository.findById(member1.getTeam().getId()).get();
+        Team findTeamB = teamRepository.findById(member3.getTeam().getId()).get();
+
+        assertThat(findTeamA).isEqualTo(member1.getTeam());
+        assertThat(findTeamB).isEqualTo(member3.getTeam());
+
+        List<Team> teamList = teamRepository.findAll();
+        assertThat(teamList.size()).isEqualTo(2);
+
+        long teamCount = teamRepository.count();
+        assertThat(teamCount).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        memberJpaRepository.delete(member3);
+        memberJpaRepository.delete(member4);
+        teamRepository.delete(teamA);
+        teamRepository.delete(teamB);
+        long deleteCount = teamRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
     }
 
 }
