6d23951db64688ccebdc05c39f7530c066eacc32
injinjeong <snowwwwww@naver.com>
2022-01-07 12:02:49 +0900

    반환타입 test case 추가

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,16 +1,18 @@
 package study.datajpa.repository;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -132,5 +134,62 @@ class MemberRepositoryTest {
         assertThat(memberDto.get(0).getUserName()).isEqualTo("AAA");
         assertThat(memberDto.get(0).getTeamName()).isEqualTo("teamA");
 
+    }
+
+    @Test
+    public void findByNames() throws Exception {
+        //given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //when
+        List<Member> result = memberRepository.findByNames(Arrays.asList(m1.getUsername(), m2.getUsername()));
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+
+        //then
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(10);
+        assertThat(result.get(1).getUsername()).isEqualTo("BBB");
+        assertThat(result.get(1).getAge()).isEqualTo(20);
+    }
+
+    @Test
+    public void returnType() throws Exception {
+        //given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 10);
+        Member m3 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        memberRepository.save(m3);
+
+        //when
+        //List<T> => 값이 없이 조회가 되도 null 이 아니라 빈 collection 을 제공해 준다.
+        List<Member> listByUsernames = memberRepository.findListByUsername(m1.getUsername());
+        List<Member> listByUsernamesIsEmpty = memberRepository.findListByUsername("CCC");
+        //단건 조회시에는 null 이 반환되므로 조심하자. 또는 2개 이상시에도 문제가 있다.
+        Member memberByUsername = memberRepository.findMemberByUsername(m3.getUsername());
+        Member memberByUsernameIsNull = memberRepository.findMemberByUsername("DDD");
+        //Optional
+        Optional<Member> optinalByusername = memberRepository.findOptinalByusername(m3.getUsername());
+        Optional<Member> optinalByusernameIsEmpty = memberRepository.findOptinalByusername("DDD");
+
+        //then
+        assertThat(listByUsernames.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(listByUsernames.get(1).getUsername()).isEqualTo("AAA");
+        assertThat(listByUsernames.size()).isEqualTo(2);
+        assertThat(listByUsernamesIsEmpty.size()).isEqualTo(0);
+
+        assertThat(memberByUsername.getUsername()).isEqualTo("BBB");
+        //현재 아래 코드를 돌리면 NonUniqResultException 이 발생하지만, spring jpa 는 springFrameWork Exception 으로 변경한다.
+        IncorrectResultSizeDataAccessException thorwn = assertThrows(IncorrectResultSizeDataAccessException.class, () ->
+                memberRepository.findMemberByUsername(m1.getUsername()));
+
+        assertThat(optinalByusername.get().getUsername()).isEqualTo("BBB");
+        assertThat(optinalByusernameIsEmpty.isEmpty()).isTrue();
     }
 }
