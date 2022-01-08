3d52fb45ef4105b00c7684e827014ca7057acd28
injinjeong <snowwwwww@naver.com>
2022-01-08 19:18:39 +0900

    드디어 querydsl init :)

--- src/test/java/study/data_querydsl_jpa/DataQueryDslJpaApplicationTest.java
+++ src/test/java/study/data_querydsl_jpa/DataQueryDslJpaApplicationTest.java
@@ -1,13 +1,43 @@
 package study.data_querydsl_jpa;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.data_querydsl_jpa.entity.Hello;
+import study.data_querydsl_jpa.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class DataQueryDslJpaApplicationTest {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        //given
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        //QHello qHello = new QHello("h");
+        QHello qHello = QHello.hello;
+        
+        //when
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        //then
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
